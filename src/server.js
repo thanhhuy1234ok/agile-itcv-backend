77650a06a63274d290b357edfd58f8e9c290416a
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2025-05-28 00:13:52 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname,'../uploads'))); 
 
 app.use('/api/v1', mainRouter);
 
